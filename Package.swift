78030bfca5ed8620ce484d94f5f8387dda0aee91
Reza Khonsari <rezakhonsari@icloud.com>
2021-08-12 22:53:48 +0430

    init

--- Package.swift
+++ Package.swift
@@ -20,7 +20,12 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RKPhotoBrowser",
-            dependencies: []),
+            dependencies: ["RKPhotoBrowserObjC"]),
+        .target(
+              name: "RKPhotoBrowserObjC",
+              dependencies: [],
+              path: "Sources/RKPhotoBrowserOBJC",
+              exclude: ["RKPhotoBrowser"]),
         .testTarget(
             name: "RKPhotoBrowserTests",
             dependencies: ["RKPhotoBrowser"]),
---
+++ Sources/RKPhotoBrowserOBJC/include/RKPhotoBrowser.h
@@ -0,0 +1,8 @@
+//
+//  Header.h
+//  
+//
+//  Created by Reza Khonsari on 8/12/21.
+//
+
+#include "../SKPhotoBrowser.h"
