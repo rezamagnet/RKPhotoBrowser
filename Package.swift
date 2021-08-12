469776312a5e39f8eba3ace3aebbabfa01718192
Reza Khonsari <rezakhonsari@icloud.com>
2021-08-12 23:09:22 +0430

    update: package

--- Package.swift
+++ Package.swift
@@ -20,7 +20,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RKPhotoBrowser",
-            dependencies: ["RKPhotoBrowserObjC"]),
+            dependencies: ["RKPhotoBrowserObjC"],
+            resources: [.process("SKPhotoBrowser.bundle")]),
         .target(
               name: "RKPhotoBrowserObjC",
               dependencies: [],
